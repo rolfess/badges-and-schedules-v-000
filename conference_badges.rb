3ef304bead67dfa6274283f18fc68e31ffb24f98
Susan Rolfes <rolfes57069@yahoo.com>
2018-09-10 02:20:07 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+#attendees = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
+#rooms = [1,2,3,4,5,6,7]
+def badge_maker (name)
+   "Hello, my name is #{name}."
+end
+
+def batch_badge_creator (attendees)
+  attendees.collect do |name|
+    badge_maker (name)
+end
+
+def assign_rooms (attendees, room)
+  puts "Hello, #{names}! You'll be assigned to room #{room}!"
+end
